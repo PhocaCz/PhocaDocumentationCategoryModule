6a16f177a7051bcd95edd34f39298f7c3805cc13
Jan <info@phoca.cz>
2017-07-03 11:18:29 +0200

    3.0.2
    Info: https://phoca.cz/phoca-documentation-category-module
    https://www.phoca.cz/news

--- mod_phocadocumentation_category.php
+++ mod_phocadocumentation_category.php
@@ -18,71 +18,114 @@ $userLevels	= implode (',', $user->getAuthorisedViewLevels());
 $menu 		= $app->getMenu();
 JHTML::stylesheet('media/com_phocadocumentation/css/phocadocumentation-transparent.css' );
 
-
-$wheres		= array();
-
+$option				= $app->input->get('option');
+$view				= $app->input->get('view');
+$id					= $app->input->get('id', 0);
+$catid				= $app->input->get('catid', 0);
 
 $display_categories 		= $params->get('display_categories', '');
 $hide_categories 			= $params->get('hide_categories', '');
 $display_empty_categories 	= $params->get('display_empty_categories', 0);
+$display_article_list 		= $params->get('display_article_list', 0);
 
-if ( $display_categories != '' ) {
-	$category_ids_where = " AND s.id IN (".$display_categories.")";
-} else {
-	$category_ids_where = '';
-}
+$wheres		= array();
 
-if ( $hide_categories != '' ) {
-	$category_ids_not_where = " AND s.id NOT IN (".$hide_categories.")";
-} else {
-	$category_ids_not_where = '';
-}
 
-$wheres[] = " cc.parent_id = 1";
-$wheres[] = " cc.published = 1";
-$wheres[] = " cc.extension = 'com_content'";
 
-if ($display_empty_categories == 2) {
-	$wheres[] = " cc.title <> 'Uncategorised'";
-} else if ($display_empty_categories == 1) {
+if ($option == 'com_content' && $view == 'article' && $display_article_list == 1) {
 	
-} else {
-	//$wheres[] = " c.state = 1";
-}
-$wheres[] = " cc.access IN (".$userLevels.")";
-
-
-
-$categoriesOrdering = 'lft';
-
-$query =  " SELECT cc.id, cc.id AS id, cc.title, cc.description, cc.alias, cc.access"
-		. " FROM #__categories AS cc"
-		//. " LEFT JOIN #__content AS c ON c.catid = cc.id"
-		. " WHERE " . implode( " AND ", $wheres )
-		. " GROUP BY cc.id"
-		. " ORDER BY cc.".$categoriesOrdering;
+	if ($catid > 0) {
 		
-$db->setQuery( $query );
-$categories = $db->loadObjectList();
-
-// ITEMID
-$itemCategory			= $menu->getItems('link', 'index.php?option=com_phocadocumentation&view=categories');
+		$wheres[] = " a.state = 1";
+		$wheres[] = " c.published = 1";
+		$wheres[] = " a.catid = ".(int)$catid;
+		$query =  " SELECT a.id, a.alias, a.title, c.id as categoryid, c.alias as categoryalias"
+			. " FROM #__content AS a"
+			. " LEFT JOIN #__categories AS c ON a.catid = c.id"
+			. " WHERE " . implode( " AND ", $wheres )
+			. " GROUP BY a.id"
+			. " ORDER BY a.id";
+		$db->setQuery( $query );
+		$articles = $db->loadObjectList();
 	
-if(isset($itemCategory[0])) {
-	$itemId = $itemCategory[0]->id;
+		
+	
+		$output = '<div id="ph-pmod-box-module">';
+		if (!empty($articles)) {
+			foreach ($articles as $value) {
+				$output .= '<div class="ph-document"><span class="glyphicon glyphicon-book"></span> ';
+				$output .= '<a href="'. JRoute::_(PhocaDocumentationHelperRoute::getArticleRoute($value->id, $value->alias, $value->categoryid,$value->categoryalias)).'">'. $value->title.'</a></div>';
+				//$output .= ' <small>('.$value->numcat.')</small></p>';
+			}	
+		}
+		$output .= '</div>';
+		
+	}
+	
 } else {
-	$itemId = JRequest::getVar('Itemid', 1, 'get', 'int');
+	
+	if ( $display_categories != '' ) {
+		$wheres[] =  "  cc.id IN (".$display_categories.")";
+	}
+
+	if ( $hide_categories != '' ) {
+		$wheres[] =  " cc.id NOT IN (".$hide_categories.")";
+	}
+
+	$wheres[] = " cc.parent_id = 1";
+	$wheres[] = " cc.published = 1";
+	$wheres[] = " cc.extension = 'com_content'";
+
+	if ($display_empty_categories == 2) {
+		$wheres[] = " cc.title <> 'Uncategorised'";
+	} else if ($display_empty_categories == 1) {
+		
+	} else {
+		//$wheres[] = " c.state = 1";
+	}
+	$wheres[] = " cc.access IN (".$userLevels.")";
+
+
+
+	$categoriesOrdering = 'lft';
+
+	$query =  " SELECT cc.id, cc.id AS id, cc.title, cc.description, cc.alias, cc.access"
+			. " FROM #__categories AS cc"
+			//. " LEFT JOIN #__content AS c ON c.catid = cc.id"
+			. " WHERE " . implode( " AND ", $wheres )
+			. " GROUP BY cc.id"
+			. " ORDER BY cc.".$categoriesOrdering;
+			
+	$db->setQuery( $query );
+	$categories = $db->loadObjectList();
+
+	// ITEMID
+	$itemCategory			= $menu->getItems('link', 'index.php?option=com_phocadocumentation&view=categories');
+		
+	if(isset($itemCategory[0])) {
+		$itemId = $itemCategory[0]->id;
+	} else {
+		$itemId = JRequest::getVar('Itemid', 1, 'get', 'int');
+	}
+
+	$output = '<div id="ph-pmod-box-module">';
+	if (!empty($categories)) {
+		foreach ($categories as $value) {
+			$output .= '<div class="ph-category"><span class="glyphicon glyphicon-folder-close"></span> ';
+			$output .= '<a href="'. JRoute::_('index.php?option=com_phocadocumentation&view=category&id='.$value->id.':'.$value->alias.'&Itemid='.(int)$itemId ).'">'. $value->title.'</a></div>';
+			//$output .= ' <small>('.$value->numcat.')</small></p>';
+		}	
+	}
+	$output .= '</div>';
+	
 }
 
-$output = '<ul id="ph-pmod-box-module">';
-if (!empty($categories)) {
-	foreach ($categories as $value) {
-		$output .= '<li class="ph-pdmod-categories">';
-		$output .= '<a href="'. JRoute::_('index.php?option=com_phocadocumentation&view=category&id='.$value->id.':'.$value->alias.'&Itemid='.(int)$itemId ).'">'. $value->title.'</a></li>';
-		//$output .= ' <small>('.$value->numcat.')</small></p>';
-	}	
-}
-$output .= '</ul>';
+
+
+
+
+
+
 
 require(JModuleHelper::getLayoutPath('mod_phocadocumentation_category'));
 ?>
